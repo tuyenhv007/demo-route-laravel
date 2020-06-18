4c51e5de7dd0e3ffd3d9c143f75adaf0ba018d29
tuyenhoang0320i1 <hoangtuyen9x@gmail.com>
2020-06-18 09:24:34 +0700

    demo route

---
+++ resources/views/user.blade.php
@@ -0,0 +1,12 @@
+<!doctype html>
+<html lang="{{str_replace('_', '-', app()->getLocale())}}">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <title>Laravel Route</title>
+</head>
+<body>
+<h1>Welcome <?= $name ?>!</h1>
+</body>
+</html>
